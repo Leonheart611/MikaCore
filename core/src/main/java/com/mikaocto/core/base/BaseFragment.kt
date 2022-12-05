cb3eb1b9dd230b2aa93f3c844974d1dceca225c3
Your Name <mika.leonheart6@gmail.com>
2022-12-05 15:45:18 +0700

    Initial commit

---
+++ core/src/main/java/com/mikaocto/core/base/BaseFragment.kt
@@ -0,0 +1,9 @@
+package com.mikaocto.core.base
+
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<view: ViewBinding>: Fragment() {
+
+
+}
