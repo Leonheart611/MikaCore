48a9bdce88c8a7d7523ac3ff4db380dd1de0675e
LeonHeart <mika.leonheart6@gmail.com>
2022-12-05 15:51:21 +0700

    create base fragment binding

--- core/src/main/java/com/mikaocto/core/base/BaseFragment.kt
+++ core/src/main/java/com/mikaocto/core/base/BaseFragment.kt
@@ -1,9 +1,31 @@
 package com.mikaocto.core.base
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseFragment<view: ViewBinding>: Fragment() {
+typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
 
+abstract class BaseFragmentBinding<view : ViewBinding>(private val inflate: Inflate<view>) :
+    Fragment() {
+    private var _viewBinding: view? = null
 
+    val viewBinding by lazy { _viewBinding!! }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _viewBinding = inflate.invoke(inflater, container, false)
+        return viewBinding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _viewBinding = null
+    }
 }
